b6044f87bb771cb10717a446917eeb20cbdaee44
rasheed <rasheedandrew1@gmail.com>
2023-05-26 08:42:50 -0400

    Initial Commit

---
+++ Rasheed_Andrew_JPMC_Weather_Challenge/Rasheed_Andrew_JPMC_Weather_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rasheed_Andrew_JPMC_Weather_ChallengeApp.swift
+//  Rasheed_Andrew_JPMC_Weather_Challenge
+//
+//  Created by rasheed andrew on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Rasheed_Andrew_JPMC_Weather_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
